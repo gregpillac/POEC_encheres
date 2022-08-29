e34e6284f7ae8435e02e83a7f3425b41f6ca7bd7
gregpiac <gregory.pillac@gmail.com>
2022-08-29 10:59:53 +0200

    initialisation du projet MySQL - JPA - SpringWeb

---
+++ src/test/java/fr/eni/ecole/projetEncheres/PoecProjetEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.ecole.projetEncheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoecProjetEncheresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
