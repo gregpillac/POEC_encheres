4790c477603ad76b36c9f6b5dc0d9d9c3e51e854
Sébastien BODIN <sebastien.bodin2022@campus-eni.fr>
2022-08-29 16:56:37 +0200

    version moi 2

--- src/main/java/fr/eni/ecole/projetEncheres/PoecProjetEncheresApplication.java
+++ src/main/java/fr/eni/ecole/projetEncheres/PoecProjetEncheresApplication.java
@@ -2,8 +2,12 @@ package fr.eni.ecole.projetEncheres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("fr.eni.ecole.projetEncheres.*")
+@EntityScan("fr.eni.ecole.projetEncheres.*")
 public class PoecProjetEncheresApplication {
 
 	public static void main(String[] args) {
--- src/main/java/fr/eni/ecole/projetEncheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/ecole/projetEncheres/bo/Utilisateur.java
@@ -5,9 +5,10 @@ import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
-import org.springframework.data.annotation.Id;
+
 
 @Entity
 public class Utilisateur {
