d9af2e00958ccb63cbc349685c6aa7d007812d42
gregpiac <gregory.pillac@gmail.com>
2022-08-29 14:38:27 +0200

    creation des bo

---
+++ src/main/java/fr/eni/ecole/projetEncheres/bo/ArticleVendu.java
@@ -0,0 +1,93 @@
+package fr.eni.ecole.projetEncheres.bo;
+
+import java.time.LocalDate;
+
+public class ArticleVendu {
+	
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEncheres;
+	private LocalDate dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private String etatVente;
+	
+	public ArticleVendu() {
+	}
+	
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			int miseAPrix, int prixVente, String etatVente) {
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+	public ArticleVendu(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, int miseAPrix, int prixVente, String etatVente) {
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+	public int getPrixVente() {
+		return prixVente;
+	}
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+	public String getEtatVente() {
+		return etatVente;
+	}
+	public void setEtatVente(String etatVente) {
+		this.etatVente = etatVente;
+	}
+	
+	
+
+}
---
+++ src/main/java/fr/eni/ecole/projetEncheres/bo/Categorie.java
@@ -0,0 +1,5 @@
+package fr.eni.ecole.projetEncheres.bo;
+
+public class Categorie {
+
+}
